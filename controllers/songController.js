afcc42b17fbdf04db9cc0a375cadd70ba5eb1dcf
besufikad17 <besufikadtialhun1994@gmail.com>
2023-02-27 19:18:03 +0300

    basic CRUD operation implemented

--- controllers/songController.js
+++ controllers/songController.js
@@ -16,7 +16,7 @@ songController.addSong = async (req, res) => {
   try {
     const newSong = await prisma.song.create({
       data: {
-        img_url,
+        url: img_url,
         title,
         artist,
       },
@@ -37,10 +37,94 @@ songController.addSong = async (req, res) => {
   }
 };
 
-songController.getSongs = async (req, res) => {};
+songController.getSongs = async (req, res) => {
+  try {
+    const songs = await prisma.song.findMany({
+      where : {}
+    })
+    res.json({
+      success: true,
+      data: songs,
+      error: null,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      data: null,
+      error: error.meta || { msg: "Error occurred check server log!" },
+    });
+  }
+};
 
-songController.updateSongById = async (req, res) => {};
+songController.updateSongById = async (req, res) => {
+  const id = req.params.id;
+  const { img_url, title, artist } = req.body;
 
-songController.deleteSongById = async (req, res) => {};
+  if (!id || !img_url || !title || !artist) {
+    return res.json({
+      success: false,
+      data: null,
+      error: { msg: "Please enter all fields properly!!" },
+    });
+  }
+  try {
+    await prisma.song.updateMany({
+      where: {
+        id
+      },
+      data: {
+        url: img_url,
+        title,
+        artist,
+      },
+    });
+
+    res.json({
+      success: true,
+      data: {msg: "Done!!"},
+      error: null,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      data: null,
+      error: error.meta || { msg: "Error occurred check server log!" },
+    });
+  }
+};
+
+songController.deleteSongById = async (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return res.json({
+      success: false,
+      data: null,
+      error: { msg: "Please enter all fields properly!!" },
+    });
+  }
+  try {
+    await prisma.song.deleteMany({
+      where: {
+        id
+      }
+    });
+
+    res.json({
+      success: true,
+      data: {msg: "Done!!"},
+      error: null,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      data: null,
+      error: error.meta || { msg: "Error occurred check server log!" },
+    });
+  }
+};
 
 module.exports = songController;
